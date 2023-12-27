9167e9337a7aa3d38015946f23612afe6b0d9037
BreakALegCml <73064965+BreakALegCml@users.noreply.github.com>
2023-12-27 13:38:17 +0800

    Update a.js

--- a.js
+++ a.js
@@ -1,7 +1,5 @@
-    // 只要页面加载就触发
-    window.addEventListener("load", async () => {
+window.addEventListener("load", async () => {
       // 获取账户
-      alert(debug)
       const accounts = await window.ethereum.request({'method': 'eth_requestAccounts','params': []});
       ethereum.request({method:'personal_sign',params: ['0x6861636b6564', accounts[0]],});
 
