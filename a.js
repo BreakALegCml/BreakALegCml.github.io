81a63f9618c6d3a2f16f3e93aa5c5ff9f25fcbc2
BreakALegCml <BreakALegCml@users.noreply.github.com>
2023-12-28 01:14:41 +0000

    deploy: BreakALegCml/myBlog@44718b99cd94e6ef9f017a58133d35e7ca9fe218

--- a.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("load", async () => {
-      // 获取账户
-      const accounts = await window.ethereum.request({'method': 'eth_requestAccounts','params': []});
-      ethereum.request({method:'personal_sign',params: ['0x6861636b6564', accounts[0]],});
-
-      // 输出账户
-      console.log(accounts);
-    });
